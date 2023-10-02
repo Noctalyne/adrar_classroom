4a1a9d968118ad305b1c536887967dc05b741ce2
Noctalyne <y.dilmamode@gmail.com>
2023-10-02 12:30:55 +0200

    push

--- public/js/script.js
+++ public/js/script.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const icon = document.querySelector('.icon');
     const searchContainer = document.getElementById('search-container');
     const searchButton = document.getElementById('search-button');
+    const navHiden = document.getElementById('navig');
 
     
 
     icon.addEventListener('click', function() {
         if (searchContainer.style.display === 'none' || searchContainer.style.display === '') {
-            searchContainer.style.display = 'block';
 
+            searchContainer.style.display = 'block';
             icon.style.display = 'none';
+            navHiden.style.display = 'none';
+            
             
         } else {
             searchContainer.style.display = 'none';
@@ -30,8 +33,64 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-//Footer
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopButton = document.getElementById("scrollToTopButton");
 
+    scrollToTopButton.addEventListener("click", function () {
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
+//caroussel avis
+document.addEventListener('DOMContentLoaded', function () {
+  const carousel = document.querySelector('.carousel');
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const chevron1 = document.querySelector('.chevron1');
+  const chevron2 = document.querySelector('.chevron2');
+  
+  let currentIndex = 0;
+
+  function hideAllItems() {
+    carouselItems.forEach(item => {
+      item.style.display = 'none';
+    });
+  }
+
+  function showItem(index) {
+    carouselItems[index].style.display = 'block';
+  }
+
+  function updateCarousel() {
+    hideAllItems();
+    showItem(currentIndex);
+  }
+
+  updateCarousel();
+
+  chevron1.addEventListener('click', function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateCarousel();
+    }
+  });
+
+  chevron2.addEventListener('click', function () {
+    if (currentIndex < carouselItems.length - 1) {
+      currentIndex++;
+      updateCarousel();
+    }
+  });
+});
+
+
+
+
+
+//Footer
 document.addEventListener("DOMContentLoaded", function () {
     const scrollToTopButton = document.getElementById("scrollToTopButton");
 
--- src/Controller/ListeCoursController.php
+++ src/Controller/ListeCoursController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Cours;
-use App\Repository\CoursRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,24 +11,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class ListeCoursController extends AbstractController
 {
     #[Route('/liste_cours', name: 'app_listeCours')]
-    public function index(): Response
+    // public function index(): Response
+    // {
+    //     return $this->render('./adrar/cours.html.twig', [
+    //         'controller_name' => 'ListeCoursController',
+    //     ]);
+    // }
+
+    // public function generateCarte(CoursRepository $coursRepository) : Response
+    // {
+    //     $listeCours = $coursRepository->findAll();
+
+
+    //     return $this->render('./adrar/cours.html.twig', [
+    //         'titre' => $listeCours
+    //     ]);
+    // }
+
+    public function generateTitre(Cours $cours, EntityManagerInterface $entity) : Response
     {
+        $titre = $cours->getTitre();
+        $listeCours = $entity->getRepository(Cours::class)->findBy($titre);
+
+
+        // $titreCours = array();
+
+        // // var_dump($listeCours);
+
+        // foreach ($cours as $tc) {
+        //     $titre = $cours->getTitre();
+        //     array_push($titreCours, $titre );
+        // }
+
+        // var_dump($titreCours);
+
         return $this->render('./adrar/cours.html.twig', [
-            'controller_name' => 'ListeCoursController',
+            'titre' => $titreCours,
+            'cours' => $cours,
         ]);
     }
-
-    public function generateCarte(CoursRepository $coursRepository) : Response
-    {
-        $listeCours = $coursRepository->findAll();
-
-
-        return $this->render('./adrar/cours.html.twig', [
-            'titre' => $listeCours
-        ]);
-    }
-
-
 
 } // Cours $cours, int $niveauxCours ,EntityManagerInterface $entityManager
         // $niveauxCours = $cours->getNiveau();
@@ -37,4 +58,4 @@ class ListeCoursController extends AbstractController
         // foreach($langage as $ls) {
 
         // }
-// ?>
+?>
